c81763a296b6fe9d274557f88a1ae3d40cd64645
Bartosz Piekny <bpiekny@luxoft.com>
2021-04-21 09:35:58 +0200

    Add c++14 samples

---
+++ c++14/include/binary_literals.h
@@ -0,0 +1,6 @@
+#pragma once
+
+bool binary_literal(){
+    constexpr int b = 0b101010;
+    return b;
+}
---
+++ c++14/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "binary_literals.h"
+
+int main(){
+    std::cout << binary_literal() << std::endl;
+}
