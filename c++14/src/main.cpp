7a6570e3df9c9e78b3eb8a9d6589b816dd2ca437
Bartosz Piekny <bpiekny@luxoft.com>
2021-04-22 20:06:19 +0200

    Add variable template and c++14 std lib features

--- c++14/src/main.cpp
+++ c++14/src/main.cpp
@@ -2,6 +2,8 @@
 #include "literals.h"
 #include "decltype_auto.h"
 #include "generalized_lambda_capture.h"
+#include "variable_template.h"
+#include "std_lib.h"
 
 
 int main(){
@@ -11,4 +13,8 @@ int main(){
     literals::main_();
     std::cout << "==== generalized lambda capture ====" << std::endl;
     generalized_lambda_captures::main_();
+    std::cout << "==== variable template ====" << std::endl;
+    variable_template::main_();
+    std::cout << "==== std_lib ====" << std::endl;
+    std_lib::main_();
 }
